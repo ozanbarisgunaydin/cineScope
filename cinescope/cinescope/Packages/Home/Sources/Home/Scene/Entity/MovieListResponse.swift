8d83750bcdaacb6dfe4954af9310c54dacc4eb76
Ozan Barış Günaydın <ozangunaydin61@gmail.com>
2024-08-11 01:41:34 +0300

    CS-03: Movie list response model added

---
+++ cinescope/cinescope/Packages/Home/Sources/Home/Scene/Entity/MovieListResponse.swift
@@ -0,0 +1,49 @@
+//
+//  MovieListResponse.swift
+//
+//
+//  Created by Ozan Barış Günaydın on 10.08.2024.
+//
+
+import Foundation
+
+// MARK: - MovieListResponse
+struct MovieListResponse: Codable {
+    let page: Int?
+    let results: [Movie]?
+}
+
+// MARK: - Movie
+struct Movie: Codable {
+    let adult: Bool?
+    let backdropPath: String?
+    let genreIDS: [Int]?
+    let id: Int?
+    let originalLanguage: String?
+    let originalTitle: String?
+    let overview: String?
+    let popularity: Double?
+    let posterPath: String?
+    let releaseDate: String?
+    let title: String?
+    let video: Bool?
+    let voteAverage: Double?
+    let voteCount: Int?
+    
+    enum CodingKeys: String, CodingKey {
+        case adult
+        case backdropPath = "backdrop_path"
+        case genreIDS = "genre_ids"
+        case id
+        case originalLanguage = "original_language"
+        case originalTitle = "original_title"
+        case overview
+        case popularity
+        case posterPath = "poster_path"
+        case releaseDate = "release_date"
+        case title
+        case video
+        case voteAverage = "vote_average"
+        case voteCount = "vote_count"
+    }
+}
