df6b8021cd9b126291855d289094f6591aedc783
Ozan Barış Günaydın <ozangunaydin61@gmail.com>
2024-08-11 01:42:36 +0300

    CS-03: Theme mode variables remved

--- cinescope/cinescope/Packages/AppManagers/Sources/AppManagers/User/UserManager.swift
+++ cinescope/cinescope/Packages/AppManagers/Sources/AppManagers/User/UserManager.swift
@@ -21,36 +21,4 @@ public class UserManager: UserManagerProtocol {
     private init() { }
 
     // MARK: - App General
-    public var isInDarkTheme: Bool {
-        get { userDefaults.isInDarkTheme }
-        set {
-            userDefaults.isInDarkTheme = newValue
-            overrideApplicationThemeStyle()
-        }
-    }
-}
-
-// MARK: - Helpers
-private extension UserManager {
-    final func overrideApplicationThemeStyle() {
-        guard let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene,
-              let window = windowScene.windows.first(where: { $0.isKeyWindow }) else { return }
-        window.overrideUserInterfaceStyle = UserManager.shared.isInDarkTheme ? .dark : .light
-    }
-}
-
-// MARK: - UserDefaults
-private extension UserDefaults {
-    private enum Keys {
-        static let isInDarkTheme: String = "isInDarkTheme"
-    }
-
-    var isInDarkTheme: Bool {
-        get {
-            return read(Bool.self, with: Keys.isInDarkTheme) ?? false
-        }
-        set {
-            save(item: newValue, forKey: Keys.isInDarkTheme)
-        }
-    }
 }
--- cinescope/cinescope/Packages/AppResources/Sources/AppResources/Protocols/UserManagerProtocol.swift
+++ cinescope/cinescope/Packages/AppResources/Sources/AppResources/Protocols/UserManagerProtocol.swift
@@ -7,6 +7,4 @@
 
 import Foundation
 
-public protocol UserManagerProtocol {
-    var isInDarkTheme: Bool { get set }
-}
+public protocol UserManagerProtocol { }
